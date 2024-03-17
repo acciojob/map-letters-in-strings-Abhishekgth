e268de4bbe97380f192cb93f0fb0b7fcb4fa95f6
Abhishekgth <128219073+Abhishekgth@users.noreply.github.com>
2024-03-17 15:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const mapLetters = (str) => {
+
+  const obj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    obj[char] = obj[char] || [];
+    obj[char].push(i);
+  }
+
+  return obj;
+};
+
+
+
+const input = prompt("Enter the string");
+console.log(mapLetters(input.trim()));
